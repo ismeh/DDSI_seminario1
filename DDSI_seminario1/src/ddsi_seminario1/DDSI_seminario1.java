414337d5cc578f43957e1bd345ccd3f4f312b0dc
ismeh <e.gadeismael@go.ugr.es>
2022-11-02 17:30:12 +0100

    conexion BD

--- DDSI_seminario1/src/ddsi_seminario1/DDSI_seminario1.java
+++ DDSI_seminario1/src/ddsi_seminario1/DDSI_seminario1.java
@@ -5,6 +5,11 @@
  */
 package ddsi_seminario1;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author Ismael
@@ -16,6 +21,44 @@ public class DDSI_seminario1 {
      */
     public static void main(String[] args) {
         System.out.println("Todo correcto");
+        try
+        {
+            //Se carga el driver JDBC
+            DriverManager.registerDriver( new oracle.jdbc.driver.OracleDriver() );
+             
+            //nombre del servidor
+            String nombre_servidor = "oracle0.ugr.es";
+            //numero del puerto
+            String numero_puerto = "1521";
+            //SID
+            String sid = "practbd.oracle0.ugr.es";
+            //URL "jdbc:oracle:thin:@nombreServidor:numeroPuerto:SID"
+            String url = "jdbc:oracle:thin:@" + nombre_servidor + ":" + numero_puerto + ":" + sid;
+ 
+            //Nombre usuario y password
+            String usuario = "DBAP1";
+            String password = "proyecto1";
+ 
+            //Obtiene la conexion
+            Connection conexion = DriverManager.getConnection( url, usuario, password );
+             
+            //Para realiza una consulta
+            Statement sentencia = conexion.createStatement();
+            ResultSet resultado = sentencia.executeQuery( "SELECT * FROM TIPO_OPERACION" );
+             
+            //Se recorre el resultado obtenido
+            while ( resultado.next() )
+            {
+                //Se imprime el resultado colocando
+                //Para obtener la primer columna se coloca el numero 1 y para la segunda columna 2 el numero 2
+                System.out.println ( resultado.getInt( 1 ) + "\t" + resultado.getString( 2 ) );
+            }
+             
+            //Cerramos la sentencia
+            sentencia.close();
+        }catch( Exception e ){
+            e.printStackTrace();
+        }
     }
     
 }
