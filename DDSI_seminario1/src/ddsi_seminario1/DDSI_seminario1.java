071d46bb6214ca39f3e6aa55005bb07793252ffe
javigp2002 <javigp2002@gmail.com>
2022-11-04 02:21:37 +0100

    passwdprop

--- DDSI_seminario1/src/ddsi_seminario1/DDSI_seminario1.java
+++ DDSI_seminario1/src/ddsi_seminario1/DDSI_seminario1.java
@@ -17,21 +17,11 @@ import java.sql.ResultSet;
  */
 public class DDSI_seminario1 {
 
-    /**
-     * @param args the command line arguments
-     */
-
-    static String nombre_servidor = "oracle0.ugr.es";
-    //numero del puerto
-    static String numero_puerto = "1521";
-    //SID
-    static String sid = "practbd.oracle0.ugr.es"; //practbd.oracle0.ugr.es
-    //URL "jdbc:oracle:thin:@nombreServidor:numeroPuerto:SID"
     static String url = "jdbc:oracle:thin:@//oracle0.ugr.es:1521/practbd.oracle0.ugr.es";
 
     //Nombre usuario y password
-    static String usuario = "x8004699";
-    static String password = "x8004699";
+    static String usuario = "x";
+    static String password = "x";
     public static void main(String[] args) {
         // TODO code application logic here
 
