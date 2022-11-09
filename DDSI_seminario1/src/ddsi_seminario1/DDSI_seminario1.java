405f32abb696327f9cdce5e699672c51414bb1dc
ismeh <gadeismael@hotmail.com>
2022-11-09 15:55:33 +0100

    Clase interfaz - Interfaz dar de alta pedido

--- DDSI_seminario1/src/ddsi_seminario1/DDSI_seminario1.java
+++ DDSI_seminario1/src/ddsi_seminario1/DDSI_seminario1.java
@@ -17,7 +17,7 @@ import java.util.Properties;
 import java.util.Scanner;
 
 import static ddsi_seminario1.FuncionesSQL.addPedido;
-
+import static ddsi_seminario1.interfazTexto.eligeOpcion;
 
 
 public class DDSI_seminario1 {
@@ -58,85 +58,12 @@ public class DDSI_seminario1 {
 
             conexion.close();
 
-
-
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println(ex.toString());
         }
 
     }
 
-    static void eligeOpcion() {
-        char numero='0';
-        while (numero != '4') {
-            System.out.println("\t(1) Borrado y nueva creación de las tablas e inserción de 10 " +
-                    "tuplas predefinidas en el código en la tabla Stock.");
-
-            System.out.println("\t(2) Dar de alta nuevo pedido");
-            System.out.println("\t(3) Mostrar contenido de las tablas de la BD");
-            System.out.println("\t(4) Salir del programa y cerrar conexión a BD");
-
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Elija una opción:");
-            numero = scanner.next().charAt(0);
-
-            switch (numero) {
-                case '1':
-                    break;
-                case '2':
-                    System.out.println("Ha elegido dar de alta un nuevo pedido");
-
-                    String fechaPedido;
-                    int codCliente, codPedido;
-
-                    System.out.println("\tIntroduzca el código del cliente");
-                    codCliente = scanner.nextInt();
-                    System.out.println("\tIntroduzca la fecha del pedido (Formato: x-x-x)");
-                    fechaPedido = scanner.nextLine();
-
-                    codPedido = addPedido(codCliente,fechaPedido);
-
-                    System.out.println("Opciones:");
-                    System.out.println("\t(1) Añadir detalles del producto");
-                    System.out.println("\t(2) Eliminar detalles del producto");
-                    System.out.println("\t(3) Cancelar pedido");
-                    System.out.println("\t(4) Finalizar pedido");
-                    char subOpcion = scanner.next().charAt(0);
-                    switch (subOpcion){
-                        case '1':
-                            //...
-                            //addDetallesPedido(codPedido);
-                            break;
-                        case '2':
-                            //deleteDetallesPedido(codPedido);
-                            break;
-                        case '3':
-                            //deletePedido(codPedido);
-                            break;
-                        case '4':
-                            //Duda, para hacer los cambios permanentes hace falta añadir detalles al pedido?
-                            //guardarCambios();
-                            break;
-                    }
-
-                    break;
-                case '3':
-                    break;
-                case '4':
-                    break;
-                default:
-                    System.out.println(ANSI_RED + "La tecla introducida no corresponde con ninguna opción válida. " +
-                            "Por favor, seleccione una de las teclas que se muestran por pantalla. Tenga en cuenta" +
-                            " que sólo se seleccionará la primera letra de la cadena que introduzca.");
-                    System.out.println(ANSI_RESET);
-                    break;
-            }
-
-//                if (numero ==1 || numero == 2 || numero == 3)
-            //mostrar contenido bd
-        }
-
-    }
 }
 
 
